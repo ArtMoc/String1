c6645bedb01ce03a79a59a74ef6e8ecf9667a3fd
ArtMoc <tem.mochalov@yandex.ru>
2021-11-23 14:32:41 +0300

    Methods out of class

--- String1/main.cpp
+++ String1/main.cpp
@@ -1,100 +1,138 @@
 ﻿//constructors_delegation_in_string
 #pragma warning (disable:4326)
 #include <iostream>
-using namespace std;
+//using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 
 #define delimiter "\n-----------------------------------------\n"
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////// ОБЪЯВЛЕНИЕ КЛАССА - CLASS DECLARATION ////////////////////////////////////////
 
 class String;
 String operator+(const String& left, const String& right);
 
 class String
 {
-	int size; //ðàçìåð ñòðîêè â áàéòàõ
-	char* str; //àäðåñ ñòðîêè â äèíàìè÷åñêîé ïàìÿòè
+	int size; //Размер строки в байтах
+	char* str; //Адрес строки в динамической памяти
 public:
-	int get_size()const
-	{
-		return size;
-	}
-	char* get_str()const
-	{
-		return str;
-	}
-	char* get_str()
-	{
-		return str;
-	}
+	int get_size()const;
+	char* get_str()const;
+	char* get_str();
 	//                         CONSTRUCTORS:
-	explicit String(int size = 80) :size(size), str(new char[size] {})
-	{
-		cout << "Default_Constructor:\t" << this << endl;
-	}
-	String(const char* str) :String(strlen(str) + 1)//Делегируем выделение памяти конструктору по умолчанию
-	{
-		for (int i = 0; i < size; i++)this->str[i] = str[i];
-		cout << "Constructor:\t\t" << this << endl;
-	}
+	explicit String(int size = 80);
+	String(const char* str);
 	//                         THE RULE OF THREE:
-	String(const String& other) :String(other.str)
-	{
-		//Deep copy
-		cout << "Copy_Constructor:\t" << this << endl;
-	}
-	String(String&& other):size(other.size), str(other.str)
-	{
-		other.str = nullptr;
-		other.size = 0;
-		cout << "Move_Constructor:\t" << this << endl;
-	}
-	~String()
-	{
-		delete[]this->str;
-		cout << "Destructor:\t\t" << this << endl;
-		//Memory leak
-	}
+	String(const String& other);
+	String(String&& other);
+	~String();
 
-	void print()const
-	{
-		cout << "Size:\t" << size << endl;
-		cout << "Str:\t" << str << endl;
-	}
+	void print()const;
 
 	//                          OPERATORS:
-	String& operator =(const String& other)
-	{
-		if (this == &other)return *this;
-		delete[] this->str;
-		//Deep copy:
-		this->size = other.size;
-		this->str = new char[size] {};
-		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
-		cout << "Copy_Assignment:\t" << this << endl;
-		return *this;
-	}
-	String& operator=(String&& other)
-	{
-		this->size = other.size;
-		this->str = other.str;
-		other.str = nullptr;
-		other.size;
-		return *this;
-	}
-	String& operator+=(const String& other)
-	{
-		return *this = *this + other;
-	}
+	String& operator =(const String& other);
+	String& operator=(String&& other);
+	String& operator+=(const String& other);
 
-	const char& operator[](int i)const
-	{
-		return str[i];
-	}char& operator[](int i)
-	{
-		return str[i];
-	}
+	const char& operator[](int i)const;
+	char& operator[](int i);
 };
 
-String operator+(const String& left, const String& right)
+/////////////////////////////////// КОНЕЦ ОБЪЯВЛЕНИЯ КЛАССА - CLASS DECLARATION END //////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//------------------------------------------------------------------------------------------------------------------
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////// ОПРЕДЕЛЕНИЕ КЛАССА - CLASS DEFINITION ////////////////////////////////////////
+
+int String::get_size()const
+{
+	return size;
+}
+char* String::get_str()const
+{
+	return str;
+}
+char* String::get_str()
+{
+	return str;
+}
+//                         CONSTRUCTORS:
+String::String(int size) :size(size), str(new char[size] {})
+{
+	cout << "Default_Constructor:\t" << this << endl;
+}
+String::String(const char* str) :String(strlen(str) + 1)//Делегируем выделение памяти конструктору по умолчанию
+{
+	for (int i = 0; i < size; i++)this->str[i] = str[i];
+	cout << "Constructor:\t\t" << this << endl;
+}
+//                         THE RULE OF THREE:
+String::String(const String& other) :String(other.str)
+{
+	//Deep copy
+	cout << "Copy_Constructor:\t" << this << endl;
+}
+String::String(String&& other) :size(other.size), str(other.str)
+{
+	other.str = nullptr;
+	other.size = 0;
+	cout << "Move_Constructor:\t" << this << endl;
+}
+String::~String()
+{
+	delete[]this->str;
+	cout << "Destructor:\t\t" << this << endl;
+	//Memory leak
+}
+
+void String::print()const
+{
+	cout << "Size:\t" << size << endl;
+	cout << "Str:\t" << str << endl;
+}
+
+//                          OPERATORS:
+String& String::operator=(const String& other)
+{
+	if (this == &other)return *this;
+	delete[] this->str;
+	//Deep copy:
+	this->size = other.size;
+	this->str = new char[size] {};
+	for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+	cout << "Copy_Assignment:\t" << this << endl;
+	return *this;
+}
+String& String::operator=(String&& other)
+{
+	this->size = other.size;
+	this->str = other.str;
+	other.str = nullptr;
+	other.size;
+	return *this;
+}
+String& String::operator+=(const String& other)
+{
+	return *this = *this + other;
+}
+
+const char& String::operator[](int i)const
+{
+	return str[i];
+}
+char& String::operator[](int i)
+{
+	return str[i];
+}
+
+//:: - Global scpoe (глобальное пространство имен)
+::String operator+(const String& left, const String& right)
 {
 	String buffer (left.get_size() + right.get_size() - 1);
 	for (int i = 0; i < left.get_size(); i++)
@@ -108,10 +146,13 @@ String operator+(const String& left, const String& right)
 	return buffer;
 }
 
-ostream& operator<<(ostream& os, const String& obj)
+std::ostream& operator<<(std::ostream& os, const String& obj)
 {
 	return os << obj.get_str();
 }
+
+/////////////////////////////////// КОНЕЦ ОПРЕДЕЛЕНИЯ КЛАССА - CLASS DEFINITION END //////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //#define CONSTRUCTORS_CHECK
 //#define OPERATOR_PLUS_CHEK
